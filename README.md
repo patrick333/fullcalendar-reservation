12684e61dc66a44f865aa84ea2906fceeae6a2ff
patrick333 <shaopeihui1@gmail.com>
2015-01-25 20:06:45 -0500

    db setting

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fullcalendar-reservation
+
+Online demo is available at http://peihuishao.com/fullcalendar-reservation
+
+# DB Setting
+A mysql database is currently used to provide the data of events. The db config setting is stored in echo_api/inc/dbcfg.inc.php. 
+
+
--- echo_api/inc/dbcfg.inc.php
+++ echo_api/inc/dbcfg.inc.php
@@ -2,38 +2,15 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-if(php_sapi_name()=='cli'){
-	$host =dirname(__FILE__);
-	$result=preg_match('#/(U_.+)/#U',$host,$matches);
-	$servername_piece=str_replace(array('U','_'), array('u',''), $matches[1],$count);
-}
-else{
-	$host = @$_SERVER['SERVER_NAME'];
-	$servername_pieces=explode(".",$host);
-	$servername_piece=$servername_pieces[0];
-}
 
+$db_cfg=array(
+	'dbname'=>'echo-driving',
+	'host'=>'localhost',
+	'user'=>'echo-driving',
+	'pass'=>'dianhua'
+	);
+break;
 
-switch ($servername_piece) {
-	case 'peihui':
-	$db_cfg=array(
-		'dbname'=>'echo-driving',
-		'host'=>'localhost',
-		'user'=>'echo-driving',
-		'pass'=>'dianhua'
-		);
-	break;
-	default:
-	$db_cfg=array(
-		'dbname'=>'echo-driving',
-		'host'=>'localhost',
-		'user'=>'echo-driving',
-		'pass'=>'dianhua'
-		);
-	break;
-
-
-}
 
 
 ?>
